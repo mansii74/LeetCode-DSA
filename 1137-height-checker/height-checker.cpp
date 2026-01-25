7ca797f27c3506d57bd18ce57d45127586993d81
mansi <119647437+mansii74@users.noreply.github.com>
2026-01-25 19:19:52 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (78.28%) - LeetSync

--- 1137-height-checker/height-checker.cpp
+++ 1137-height-checker/height-checker.cpp
@@ -2,10 +2,7 @@ class Solution {
 public:
     int heightChecker(vector<int>& heights) {
         int n= heights.size();
-        vector<int>ans(n);
-        for(int i=0;i<n;i++){
-            ans[i]=heights[i];
-        }
+        vector<int>ans =heights;
         sort(ans.begin(),ans.end());
         int count =0;
         for(int i=0;i<n;i++){
